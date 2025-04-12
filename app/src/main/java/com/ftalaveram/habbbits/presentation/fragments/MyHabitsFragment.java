c25180eebe0765ad89d0c0851bbd77371a05df8f
Fernando Talavera <ftalaveram01@gmail.com>
2025-04-12 20:11:05 +0200

    MEJORA EN PANTALLA DE CARGA AL REINICIAR HABIENDO INICIADO SESION

---
+++ app/src/main/java/com/ftalaveram/habbbits/presentation/fragments/MyHabitsFragment.java
@@ -0,0 +1,27 @@
+package com.ftalaveram.habbbits.presentation.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ftalaveram.habbbits.R;
+
+public class MyHabitsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_habits, container, false);
+    }
+}
