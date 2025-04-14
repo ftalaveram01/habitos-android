70522bad17fb3e07aa09ffb89a58d5f3c5a773d4
Fernando Talavera <ftalaveram01@gmail.com>
2025-04-14 23:07:09 +0200

    pantalla de mis habitos casi hecha

--- app/src/main/java/com/ftalaveram/habbbits/presentation/viewmodels/MyHabitsViewModel.java
+++ app/src/main/java/com/ftalaveram/habbbits/presentation/viewmodels/MyHabitsViewModel.java
@@ -24,7 +24,14 @@ public class MyHabitsViewModel extends ViewModel {
     private void loadHabits() {
         // TODO: traerlos de la api por medio del repositorio de habitos
         List<UserHabit> habits = new ArrayList<>();
-        habits.add(new UserHabit("comer", "comer todos los dias", 1L, new Date(), true));
+        habits.add(new UserHabit("comer", "comer todos los dias hasta reventar para tener el estomago bien lleno hasta reventar y ver que no es el destino es que no puedes mas ya con todo lo que me llevas encima ff fffffffff ffffffff fffffff ffff fffffffff ff ffff ff ffff f ffffffff f ffffff f fffffffff ffffff f fff ff  ffff ff f ffffff", 1L, new Date(), true));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
+        habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
         habits.add(new UserHabit("beber agua", "beber agua cada hora", 2L, new Date(), false));
 
         habitsLiveData.postValue(habits);
